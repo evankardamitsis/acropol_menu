28888d63bf06f6f2b9f659e90150c58ec8696222
Vkar90 <kardamitsis.e@gmail.com>
2022-02-15 13:46:32 +0200

    add styled components for menu items

--- components/Menu.js
+++ components/Menu.js
@@ -11,12 +11,61 @@ const Subcategory = ({ data = { menuItems: [] }, ...props }) => (
           </li>
         ))}
       </ul>
+      <style jsx>{`
+      ul,
+      h2 {
+        margin: 0;
+        text-transform: lowercase;
+      }
+      ul {
+        list-style: none;
+        padding: 0;
+      }
+      li {
+        display: flex;
+        justify-content: space-between;
+        font-size: 1rem;
+        text-transform: uppercase;
+        margin-bottom: 10px;
+        font-family: "Proxima Nova", sans-serif;
+      }
+      .subcategory {
+        display: flex;
+        flex-direction: column;
+        padding: 20px;
+        justify-content: center;
+      }
+      @media (min-width: 750px) {
+        .subcategory {
+          flex-direction: row;
+        }
+      }
+      .subcategory > h2 {
+        font-size: 3rem;
+        margin-right: 20px;
+        margin-bottom: 20px;
+      }
+      .price {
+        margin-left: 20px;
+      }
+    `}</style>
     </div>
   );
 
 const Logo = () => (
     <h1>
-      menu
+      MENU
+      <style jsx>{`
+      h1 {
+        margin: 0;
+        grid-column-start: 2;
+        padding: 20px;
+        font-family: "Proxima Nova", sans-serif;
+        letter-spacing: 10px;
+        font-weight: 300;
+        font-size: 5rem;
+      }
+    `}</style>
     </h1>
   );
 
@@ -26,18 +75,51 @@ const Logo = () => (
       {data.subcategories.map(subcategory => (
         <Subcategory key={subcategory.name} data={subcategory} />
       ))}
+      <style jsx>{`
+      .category:nth-child(even) {
+        background: #e9ebf0;
+      }
+      .category:nth-child(odd) {
+        background: #e9ebf0;
+      }
+      .category {
+        padding: 20px;
+        display: grid;
+        grid-template-columns: 1fr 90vw 1fr;
+      }
+      @media (min-width: 750px) {
+        .category {
+          grid-template-columns: 1fr 700px 1fr;
+        }
+      }
+      .category > :global(div) {
+        grid-column-start: 2;
+      }
+    `}</style>
     </section>
   );
   
 
-const Menu = ({ data = { categories: [] } }) => {
-  return (
-    <article className="menu">
-      {data.categories.map((category) => (
-        <Category key={category.name} data={category} />
-      ))}
-    </article>
-  );
-};
+  const Menu = ({ data = { categories: [] } }) => {
+    return (
+      <article className="menu">
+        {data.categories.map((category) => (
+          <Category key={category.name} data={category} />
+        ))}
+        <style jsx>{`
+          .menu {
+            color: #202021;
+            background: #eee;
+            /* System font stack: https://css-tricks.com/snippets/css/system-font-stack/ */
+            font-family: "Proxima Nova", sans-serif;
+            padding: 10px;
+            max-width: 100%;
+            overflow: hidden;
+            min-height: 100vh;git
+          }
+        `}</style>
+      </article>
+    );
+  };
 
 export default Menu;
