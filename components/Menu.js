e8e04077bfd43cd0e4c97a69d4b6b6abd1107d51
Vkar90 <kardamitsis.e@gmail.com>
2022-02-15 13:28:11 +0200

    show all data from json

--- components/Menu.js
+++ components/Menu.js
@@ -1,5 +1,35 @@
 import React from "react";
 
+const Subcategory = ({ data = { menuItems: [] }, ...props }) => (
+    <div className="subcategory" {...props}>
+      <h2>{data.name}</h2>
+      <ul>
+        {data.menuItems.map(element => (
+          <li key={element.title}>
+            {element.title}
+            <span className="price">{element.price}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+const Logo = () => (
+    <h1>
+      menu
+    </h1>
+  );
+
+  const Category = ({ data = { subcategories: [] }, ...props }) => (
+    <section className="category" {...props}>
+      <Logo />
+      {data.subcategories.map(subcategory => (
+        <Subcategory key={subcategory.name} data={subcategory} />
+      ))}
+    </section>
+  );
+  
+
 const Menu = ({ data = { categories: [] } }) => {
   return (
     <article className="menu">
