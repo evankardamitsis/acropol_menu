3abeca9ec5fb1027897b5161477d5a7cb6d830a7
Kardamitsis Evan <kardamitsis.e@gmail.com>
2022-11-28 15:36:42 +0200

    fixes

--- components/Menu.js
+++ components/Menu.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 const Subcategory = ({ data = { menuItems: [] }, ...props }) => (
-    <div className="subcategory" {...props}>
-      <h2>{data.name}</h2>
-      <ul>
-        {data.menuItems.map(element => (
-          <li key={element.description}>
-            {element.description}
-            <span className="price">{element.price}</span>
-          </li>
-        ))}
-      </ul>
-      <style jsx>{`
+  <div className="subcategory" {...props}>
+    <h2>{data.name}</h2>
+    <ul>
+      {data.menuItems.map((element) => (
+        <li key={element.description}>
+          {element.description}
+          <span className="price">{element.price}</span>
+        </li>
+      ))}
+    </ul>
+    <style jsx>{`
       ul,
       h2 {
         margin: 0;
@@ -31,24 +31,24 @@ const Subcategory = ({ data = { menuItems: [] }, ...props }) => (
       }
       @media (max-width: 1140px) {
         li {
-          font-size:0.9rem;
-          display:flex;
-          flex-wrap:wrap;
-          justify-content:start;
+          font-size: 0.9rem;
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: start;
         }
       }
       @media (min-width: 820px) {
-         .subcategory > li {
-          font-size:1rem;
-          justify-content:start;
+        .subcategory > li {
+          font-size: 1rem;
+          justify-content: start;
         }
       }
       @media (max-width: 414px) {
-         li {
+        li {
           font-size: 0.8rem;
-          justify-content:flex-start;
+          justify-content: flex-start;
           flex-wrap: nowrap;
-          text-align:center;
+          text-align: center;
           display: flow-root;
         }
       }
@@ -63,32 +63,32 @@ const Subcategory = ({ data = { menuItems: [] }, ...props }) => (
           flex-direction: column;
         }
       }
-      @media(max-width: 414px){
-          .subcategory{
-              text-align:center;
-          }
+      @media (max-width: 414px) {
+        .subcategory {
+          text-align: center;
+        }
       }
       .subcategory > h2 {
         font-size: 3rem;
         margin-right: 20px;
         margin-bottom: 20px;
       }
-      @media(max-width:414px){
-          .subcategory > h2 {
-              font-size:2rem;
-              margin-right:12px;
-          }
-          .price{
-             font-weight: 600; 
-             white-space: pre;  
-          }
+      @media (max-width: 414px) {
+        .subcategory > h2 {
+          font-size: 2rem;
+          margin-right: 12px;
+        }
+        .price {
+          font-weight: 600;
+          white-space: pre;
+        }
       }
       .price {
         margin-left: 20px;
       }
     `}</style>
-    </div>
-  );
+  </div>
+);
 
 // const Logo = () => (
 //     <h1>
@@ -107,18 +107,17 @@ const Subcategory = ({ data = { menuItems: [] }, ...props }) => (
 //               text-align:center;
 //           }
 //       }
-//     `}</style>  
+//     `}</style>
 //     </h1>
 //   );
-  
 
-  const Category = ({ data = { subcategories: [] }, ...props }) => (
-    <section className="category" {...props}>
-      {/* <Logo /> */}
-      {data.subcategories.map(subcategory => (
-        <Subcategory key={subcategory.name} data={subcategory} />
-      ))}
-      <style jsx>{`
+const Category = ({ data = { subcategories: [] }, ...props }) => (
+  <section className="category" {...props}>
+    {/* <Logo /> */}
+    {data.subcategories.map((subcategory) => (
+      <Subcategory key={subcategory.name} data={subcategory} />
+    ))}
+    <style jsx>{`
       .category:nth-child(even) {
         background: #ebebeb;
       }
@@ -139,28 +138,27 @@ const Subcategory = ({ data = { menuItems: [] }, ...props }) => (
         grid-column-start: 2;
       }
     `}</style>
-    </section>
-  );
-  
+  </section>
+);
 
-  const Menu = ({ data = { categories: [] } }) => {
-    return (
-      <article className="menu">
-        {data.categories.map((category) => (
-          <Category key={category.name} data={category} />
-        ))}
-        <style jsx>{`
-          .menu {
-            color: #202021;
-            background: #eee;
-            padding: 10px;
-            max-width: 100%;
-            overflow: hidden;
-            min-height: 100vh;
-          }
-        `}</style>
-      </article>
-    );
-  };
+const Menu = ({ data = { categories: [] } }) => {
+  return (
+    <article className="menu">
+      {data.categories.map((category) => (
+        <Category key={category.name} data={category} />
+      ))}
+      <style jsx>{`
+        .menu {
+          color: #202021;
+          background: #eee;
+          padding: 10px;
+          max-width: 100%;
+          overflow: hidden;
+          min-height: 100vh;
+        }
+      `}</style>
+    </article>
+  );
+};
 
 export default Menu;
--- pages/_document.js
+++ pages/_document.js
@@ -8,12 +8,11 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-        </Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
